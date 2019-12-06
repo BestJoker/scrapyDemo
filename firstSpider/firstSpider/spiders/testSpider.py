c6c18af553f99efc9d4b4b01232e8ffd7f9054c2
付金诗 <451931737@qq.com>
2019-12-06 20:07:19 +0800

    修改爬取地址

--- firstSpider/firstSpider/spiders/testSpider.py
+++ firstSpider/firstSpider/spiders/testSpider.py
@@ -6,22 +6,15 @@ class TestspiderSpider(scrapy.Spider):
     #爬虫的名字，必须是唯一的
     name = 'testSpider'
     #允许的域名，限制不会乱跑到其他域名上去了
-    allowed_domains = ['jianshu.com']
+    allowed_domains = ['itjuzi.com']
     #初始化的url地址
-    start_urls = ['http://www.jianshu.com/']
+    start_urls = ['http://www.itjuzi.com/']
 
     def parse(self, response):
+
+        print(response)
+
         item = FirstspiderItem()
-        item['content'] = response.xpath('//a[@class="title"]/text()').extract()
-        #拿到对应的详细地址后半段list
-        addList = response.xpath('//a[@class="title"]/@href').extract()
 
-        #循环创建出对应详情的地址，存储到数组中，并赋值给item【'link'】
-        array = []
-        for i in range(0,len(addList)):
-            new_url = 'http://www.jianshu.com' + addList[i]
-            array.append(new_url)
-
-        item['link'] = array
         #返回item否则不会调用items文件中的方法
         yield item
