85fbbd59beabf42c8f37c48b355d8a99a44f5ef5
付金诗 <451931737@qq.com>
2019-12-06 20:01:00 +0800

    备注

--- firstSpider/firstSpider/spiders/testSpider.py
+++ firstSpider/firstSpider/spiders/testSpider.py
@@ -12,15 +12,16 @@ class TestspiderSpider(scrapy.Spider):
 
     def parse(self, response):
         item = FirstspiderItem()
-        print(response)
         item['content'] = response.xpath('//a[@class="title"]/text()').extract()
         #拿到对应的详细地址后半段list
         addList = response.xpath('//a[@class="title"]/@href').extract()
-        array = []
+
         #循环创建出对应详情的地址，存储到数组中，并赋值给item【'link'】
+        array = []
         for i in range(0,len(addList)):
             new_url = 'http://www.jianshu.com' + addList[i]
             array.append(new_url)
+
         item['link'] = array
         #返回item否则不会调用items文件中的方法
         yield item
