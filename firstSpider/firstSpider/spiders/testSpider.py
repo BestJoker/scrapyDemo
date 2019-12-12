7ed9d9171bacef2dc43be83abc1b0316fef346be
付金诗 <451931737@qq.com>
2019-12-12 12:46:48 +0800

    豆瓣登录

--- firstSpider/firstSpider/spiders/testSpider.py
+++ firstSpider/firstSpider/spiders/testSpider.py
@@ -6,16 +6,20 @@ class TestspiderSpider(scrapy.Spider):
     #爬虫的名字，必须是唯一的
     name = 'testSpider'
     #允许的域名，限制不会乱跑到其他域名上去了
-    allowed_domains = ['ip138.com']
+    allowed_domains = ['douban.com']
 
     def start_requests(self):
-
         # 初始化的url地址
-        start_urls = ['http://2000019.ip138.com/']
-
+        start_urls = ['https://accounts.douban.com/j/mobile/login/basic']
+        name = '13940829974'
+        password = 'fjs57771529'
         #参数配置
         formdata = {
-
+            'ck' : '',
+            'name' : name,
+            'password' : password,
+            'remember' : '0',
+            'ticket' : ''
         }
         # 循环发送请求
         for url in start_urls:
@@ -24,7 +28,7 @@ class TestspiderSpider(scrapy.Spider):
 
     def parse(self, response):
         item = FirstspiderItem()
-        data = response.xpath('//body/p[1]/text()').extract()
+        data = response.text
         print('结果 %s' % data)
         #返回item否则不会调用items文件中的方法
         yield item
